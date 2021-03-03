873571f78210bfb74895f21354cee10dbd09dd7c
TotallyAm <78596531+TotallyAm@users.noreply.github.com>
2021-03-03 21:39:40 +0000

    Removed redundant log

--- commands/sabaton.js
+++ commands/sabaton.js
@@ -2,7 +2,7 @@ const ffmpeg = require('ffmpeg-static');
 const fs = require('fs');
 const path = require('path');
 
-console.log(path.join(process.cwd(), 'music'));
+
 
 module.exports = {
     name: 'sabaton',
