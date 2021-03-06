548b96f01a0d3a4ce0a5d9c7c7048539a2cb8b4e
TotallyAm <78596531+TotallyAm@users.noreply.github.com>
2021-03-06 00:56:12 +0000

    Added exponentials and a help message

--- commands/calculator.js
+++ commands/calculator.js
@@ -2,22 +2,25 @@ module.exports = {
     name: 'calculate',
     sum: null,
     execute(client, message, args, Discord) {
-        const s1 = Number(args[0]);
-        const s2 = Number(args[2]);
+        const x = Number(args[0]);
+        const y = Number(args[2]);
         const operative = args[1];
         if (args.length === 3) {
             switch (operative) {
                 case '+':
-                    this.sum = s1 + s2
+                    this.sum = x + y
                     break;
                 case '-':
-                    this.sum = s1 - s2
+                    this.sum = x - y
                     break;
                 case '*':
-                    this.sum = s1 * s2
+                    this.sum = x * y
                     break;
                 case '/':
-                    this.sum = s1 / s2
+                    this.sum = x / y
+                    break;
+                case '^':
+                    this.sum = (Math.pow(x, y));
                     break;
                 default:
                     message.channel.send(`What? I don't understand`);
@@ -25,6 +28,9 @@ module.exports = {
 
             }
             message.channel.send(`The answer is ${this.sum}, but it's not like I did it for you, BAKA!`);
+            console.log(this.sum)
+        } else if (!args.length) {
+            message.channel.send(`I can calculate basic operations between two numbers in the format 'x, *operative*, y',   :wink:`)
         } else message.channel.send('B-baka, I have no idea how to do that!');
 
 
