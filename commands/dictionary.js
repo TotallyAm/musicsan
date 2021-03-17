394b20bf62809f476d215823bbe143275a6d3cbf
TotallyAm <78596531+TotallyAm@users.noreply.github.com>
2021-03-17 00:08:18 +0000

    Added multi-language support

--- commands/dictionary.js
+++ commands/dictionary.js
@@ -1,14 +1,25 @@
 const axios = require('axios');
 module.exports = {
     name: 'define',
-    async execute(message, args) {
+    word: null,
+    language: null,
+    async execute(message, args, cmd, client, Discord) {
         {
             //openweathermap api token
-            const word = args
-            const URL = `https://api.dictionaryapi.dev/api/v2/entries/en_GB/${word}`
-            if (args[0] === 'bastard'){
-              message.channel.send('You!')
-              return
+
+            if (args.length === 1) {
+                this.language = 'en_GB'
+                this.word = args[0]
+
+            } else{
+                this.word = args[1]
+                this.language = args[0]
+            }
+            const URL = `https://api.dictionaryapi.dev/api/v2/entries/${this.language}/${this.word}`
+            console.log(URL)
+            if (args[0] === 'bastard') {
+                message.channel.send('You!')
+                return
             }
             axios
                 .get(
@@ -18,11 +29,10 @@ module.exports = {
                     let data = response.data[0]
                     let define = (data.meanings[0])
                     const meaning = (define.definitions[0])
-                    message.channel.send(`The definition for ${word} is '${meaning.definition.toLowerCase()}'`)
+                    message.channel.send(`The definition for ${this.word} is '${meaning.definition.toLowerCase()}'`)
                 }).catch(err => {
-                    message.reply(`Something went wrong, is ***${word}*** even in the dictionary? BAKA`)
+                    message.reply(`Something went wrong, ***${this.word}*** couldn't be found in the ${this.language} dictionary`)
                     console.log(err)
                 })
         }
     }
-}
