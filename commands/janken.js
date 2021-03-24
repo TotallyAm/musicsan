22ae58297072fe3d8493ef4911e050b214a6b354
TotallyAm <78596531+TotallyAm@users.noreply.github.com>
2021-03-24 00:52:01 +0000

    Now uses an algorithm to determine results

--- commands/janken.js
+++ commands/janken.js
@@ -3,46 +3,36 @@ module.exports = {
     aliases: ['rock', 'paper', 'scissors'],
     choice: null,
     result: null,
-    execute(message, args, cmd) {
-        randomNumber = Math.floor(Math.random() * 3 + 1);
+    execute(message, args, cmd, client, Discord) {
+        const randomNumber = Math.floor(Math.random() * 3);
         //convert the number to a choice
         switch (randomNumber) {
-            case 1:
+            case 0:
                 this.choice = 'rock'
                 break;
-            case 2:
+            case 1:
                 this.choice = 'paper'
                 break;
-            case 3:
+            case 2:
                 this.choice = 'scissors'
                 break;
         }
-        switch (cmd) {
-            //use the choice to determine the outcome
-            case 'rock':
-                if (this.choice === 'paper') {
-                    this.result = 'I win'
-                } else if (this.choice === cmd) {
-                    this.result = `It's a tie`
-                } else this.result = 'you win'
-                break;
 
-            case 'paper':
-                if (this.choice === 'scissors') {
-                    this.result = 'I win'
-                } else if (this.choice === cmd) {
-                    this.result = `It's a tie`
-                } else this.result = 'you win'
-                break;
-
-            case 'scissors':
-                if (this.choice === 'rock') {
-                    this.result = 'I win'
-                } else if (this.choice === cmd) {
-                    this.result = `It's a tie`
-                } else this.result = 'you win'
-                break;
+        function rps(x) {
+            if (x === 'rock') return x = 0
+            else if (x === 'paper') return x = 1
+            else if (x === 'scissors') return x = 2
         }
+        const userChoice = rps(cmd);
+
+
+        if ((userChoice + 1) % 3 === randomNumber) this.result = 'I win'
+        else if (userChoice === randomNumber) this.result = `It's a tie`
+        else this.result = 'you win'
+
+
+        console.log(this.result)
+        console.log(randomNumber)
         message.channel.send(`I chose ${this.choice}, ${this.result}!`);
 
     }
