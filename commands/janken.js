9fe5030fa1c57eb4eb2ad9ba677ed05a3e7dd4fa
TotallyAm <78596531+TotallyAm@users.noreply.github.com>
2021-04-07 20:05:13 +0100

    Refactoring
    Now uses an algorithm to determine results.

--- commands/janken.js
+++ commands/janken.js
@@ -1,39 +1,36 @@
 module.exports = {
-    name: 'rock',
-    aliases: ['rock', 'paper', 'scissors'],
-    choice: null,
-    result: null,
-    execute(message, args, cmd, client, Discord) {
-        const randomNumber = Math.floor(Math.random() * 3);
-        //convert the number to a choice
-        switch (randomNumber) {
-            case 0:
-                this.choice = 'rock'
-                break;
-            case 1:
-                this.choice = 'paper'
-                break;
-            case 2:
-                this.choice = 'scissors'
-                break;
-        }
-
-        function rps(x) {
-            if (x === 'rock') return x = 0
-            else if (x === 'paper') return x = 1
-            else if (x === 'scissors') return x = 2
-        }
-        const userChoice = rps(cmd);
-
-
-        if ((userChoice + 1) % 3 === randomNumber) this.result = 'I win'
-        else if (userChoice === randomNumber) this.result = `It's a tie`
-        else this.result = 'you win'
-
-
-        console.log(this.result)
-        console.log(randomNumber)
-        message.channel.send(`I chose ${this.choice}, ${this.result}!`);
-
+  name: 'rock',
+  aliases: ['rock', 'paper', 'scissors'],
+  choice: null,
+  result: 'you win',
+  execute(message, args, cmd, client, Discord) {
+    randomNumber = Math.floor(Math.random() * 3);
+    //generate the equivalent name of each random number generated, the numerical form is still used for the algorithm
+    switch (randomNumber) {
+      case 0:
+        this.choice = 'rock'
+        break;
+      case 1:
+        this.choice = 'paper'
+        break;
+      case 2:
+        this.choice = 'scissors'
+        break;
     }
+
+    //function to convert the command to a set numerical value equivalent of the randomNumer variable
+    function rps(x) {
+      if (x === 'rock') return x = 0
+      else if (x === 'paper') return x = 1
+      else if (x === 'scissors') return x = 2
+    }
+    const userChoice = rps(cmd);
+
+    //algorithm to determine the outcome of rock paper scissors numerically
+    if ((userChoice + 1) % 3 === randomNumber) this.result = 'I win'
+    else if (userChoice === randomNumber) this.result = `It's a tie`
+
+    message.channel.send(`I chose ${this.choice}, ${this.result}!`);
+
+  }
 }
